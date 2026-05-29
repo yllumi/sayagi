f8877f853f51766cf239b415d2390f7168b2bc2d
Toni Haryanto <toha.samba@gmail.com>
2026-05-29 10:24:56 +0700

    Fix favicon

--- src/app/view/_layouts/auth.php
+++ src/app/view/_layouts/auth.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?? 'Login' ?></title>
 
+    <link rel="shortcut icon" href="<?= setting('site.site_logo_small') ?? '' ?>" type="image/x-icon">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
